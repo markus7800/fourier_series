3607c8a3b953af0e7fa5fbfe5427355c26f32cd5
Markus Boeck <e1634838@student.tuwien.ac.at>
2020-09-04 17:39:14 +0200

    markus plot

--- main.jl
+++ main.jl
@@ -60,7 +60,7 @@ scatter(points);
 plot!(points);
 plot!(fs, legend=false)
 
-function plot_fourier_circles(coeffs::Vector{ComplexF64}, t::Float64=0.)
+function plot_fourier_circles(coeffs::Vector{ComplexF64}, t::Float64=0.; circle_color=1, center_size=2)
     depth = length(coeffs) ÷ 2
     n0 = depth + 1
     ts = LinRange(0, 2π, 100)
@@ -68,19 +68,19 @@ function plot_fourier_circles(coeffs::Vector{ComplexF64}, t::Float64=0.)
     p = plot(legend=false, size=(600,600), aspect_ratio=:equal)
 
     current = coeffs[n0]
-    scatter!([coeffs[n0]], mc=2)
+    scatter!([coeffs[n0]], mc=2, ms=center_size)
 
 
     for d in 1:depth
         n = n0 + d
-        plot!(current .+ coeffs[n] * exp.(im*d*ts), lc=1)
+        plot!(current .+ coeffs[n] * exp.(im*d*ts), lc=circle_color)
         current += coeffs[n] * exp(im*d*t)
-        scatter!([current], mc=2)
+        scatter!([current], mc=2, ms=center_size)
 
         n = n0 - d
-        plot!(current .+ coeffs[n] * exp.(-im*d*ts), lc=1)
+        plot!(current .+ coeffs[n] * exp.(-im*d*ts), lc=circle_color)
         current += coeffs[n] * exp(-im*d*t)
-        scatter!([current], mc=2)
+        scatter!([current], mc=2, ms=center_size)
     end
 
     return p
@@ -88,18 +88,45 @@ end
 
 plot_fourier_circles(cs)
 
-function animate_fourier(coeffs::Vector{ComplexF64}, n::Int=100; fps=30)
-    ts = LinRange(0, 2π, n)
+function animate_fourier(coeffs::Vector{ComplexF64}, n::Int=100; fps=30, filename="fourier.gif", trace=false,
+    circle_color=1, center_size=2, lw=3, lc=:black, t0=0, t1=2π, freeze_seconds=1)
+    ts = vcat(LinRange(t0, t1, n), fill(t1, Int(ceil(fps + freeze_seconds))))
     F = get_fourier_func(coeffs)
     fs = F.(ts)
 
     anim = Animation()
-    @progress for t in ts
-        p = plot_fourier_circles(coeffs, t)
-        plot!(fs)
+    @progress for (i,t) in enumerate(ts)
+        p = plot_fourier_circles(coeffs, t, circle_color=circle_color, center_size=center_size)
+        if trace
+            plot!(fs[1:i], lw=lw, lc=lc)
+        else
+            plot!(fs, lw=lw, lc=lc)
+        end
         frame(anim, p)
     end
-    gif(anim, pwd() * "\\fourier.gif", fps=fps)
+    gif(anim, pwd() * "\\" * filename, fps=fps)
 end
 
 animate_fourier(cs, 1000, fps=30)
+
+using CSV
+
+df = CSV.read("markus.txt")
+
+markus_points = Float64.(df[!, "x"]) .- im * df[!, "y"]
+
+scatter(markus_points)
+plot(markus_points, lw=5)
+
+markus_cs = fourier_series(markus_points, 100, 0.001)
+
+markus_F = get_fourier_func(markus_cs)
+ts = collect(0.15:0.001:2π-0.15)
+markus_fs = markus_F.(ts)
+plot(markus_fs)
+
+scatter(markus_points);
+plot!(markus_points);
+plot!(markus_fs, legend=false)
+
+animate_fourier(markus_cs, 1000, fps=30, trace=true, t0=0.15, t1=2π-0.15, filename="markus_2.gif", lw=8)
